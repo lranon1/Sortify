f6df644e16ca6aea67d58648dfb90097364e4ba4
Lia Ranon <lia.ranon@gmail.com>
2023-12-03 18:05:01 -0500

    added functions

--- main.py
+++ main.py
@@ -42,6 +42,19 @@ def search_for_artist(token, artist_name):
         return None
     return json_result[0]
 
+def search_for_track(token, track_title):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"?q={track_title}&type=track&limit=10"
+    query_url = url + query
+    result = get(query_url,headers=headers)
+    json_result = json.loads(result.content)["tracks"]["items"]
+    #print(json_result)
+    if len(json_result) == 0:
+        print("Track not found...")
+        return None
+    return json_result[0]
+
 def get_songs_by_artist(token, artist_id):
     url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
     headers = get_auth_header(token)
@@ -49,12 +62,55 @@ def get_songs_by_artist(token, artist_id):
     json_result = json.loads(result.content)["tracks"]
     return json_result
 
+def available_genres(token):
+    url = "https://api.spotify.com/v1/recommendations/available-genre-seeds"
+    headers = get_auth_header(token)
+    result = get(url,headers=headers)
+    json_result = json.loads(result.content)["genres"]
+    #print(json_result)
+    return json_result
+
+def get_recommendations(token, artist, genre, track):
+    url = "https://api.spotify.com/v1/recommendations"
+    headers = get_auth_header(token)
+    query = f"?seed_artists={artist}&seed_genres={genre}&seed_tracks={track}"
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
+
+
 token = get_token()
-result = search_for_artist(token, "ACDC")
-#print(result["name"])
-artist_id = result["id"]
+
+print("Welcome to Spotify Recommender\n")
+
+#menu turn into while
+artist, genre, track = input("To get recommendations give one artist, one genre, and one track: ").split()
+
+#for artist
+artist_name = search_for_artist(token, artist)
+#if artist_name == None: 
+print(artist_name["name"])
+artist_id = artist_name["id"]
 songs = get_songs_by_artist(token, artist_id)
 #print(songs)
 
 for idx, song in enumerate(songs):
-    print(f"{idx+1}. {song['name']}")
+    print(f"{idx+1}. {song['name']}") 
+
+
+#for genre
+genres = available_genres(token)
+if genre not in genres:
+    print("No such genre exists...\n")
+
+#for track
+track_title = search_for_track(token, track)
+#if track_title == None:
+print(track_title["name"])
+track_id = track_title["id"]
+
+recommendations = get_recommendations(token, artist_id, genre, track_id)
+for idx, song in enumerate(recommendations):
+    print(f"{idx+1}. {song['name']}") 
+
